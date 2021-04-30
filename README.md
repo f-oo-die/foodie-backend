68729b630932b7dc3149e0d4beca57a2265ff09f
Armin Salihovic <armin.salihovic@live.com>
2021-04-30 11:36:22 +0200

    database setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Foodie
+
+
+### Connecting to the database: 
+1. Create a database with name
+   - `foodie`
+2. Set local environment variables
+   - `APPLICATION_PORT` _(e.g. 8083)_
+   - `APPLICATION_DB_USER` _(e.g. postgres)_
+   - `APPLICATION_DB_PASSWORD` _(e.g. 1234)_
---
+++ src/main/java/com/foodie/api/model/entities/User.java
@@ -0,0 +1,41 @@
+package com.foodie.api.model.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "weight", nullable = false)
+    private Integer weight;
+
+    @Column(name = "height", nullable = false)
+    private Integer height;
+
+    @Column(name = "gender")
+    private String gender;
+}
