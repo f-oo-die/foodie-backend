9d88b8c3c1171f59496ae095ee1afa676a81cd20
Tarik Heleg <tarik.heleg@stu.sst.edu.ba>
2021-05-02 19:26:12 +0200

    Attempt at creating Controller, DTO, entity, repo and service for Ingredient

---
+++ src/main/java/com/foodie/api/controller/IngredientController.java
@@ -0,0 +1,58 @@
+package com.foodie.api.controller;
+
+import java.util.Collection;
+
+
+import com.foodie.api.model.dto.IngredientDto;
+import com.foodie.api.service.IngredientService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/ingredients")
+@RequiredArgsConstructor
+public class IngredientController {
+
+    private final IngredientService service;
+
+    @GetMapping
+    public ResponseEntity<Collection<IngredientDto>> getAll(){
+        Collection<IngredientDto> allIngredients = service.getAll();
+    
+        return ResponseEntity.status(HttpStatus.OK).body(allIngredients);
+      }
+      
+      @GetMapping("/{id}")
+      public ResponseEntity<IngredientDto> get(
+        @PathVariable Long id
+      ){
+        IngredientDto result = service.getIngredient(id);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+  
+    @PostMapping
+    public ResponseEntity<IngredientDto> save(
+        @RequestBody IngredientDto ingredient
+      ){
+        IngredientDto result = service.save(ingredient);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<IngredientDto> update(
+        @PathVariable Long id,
+        @RequestBody IngredientDto ingredient
+        ){
+            IngredientDto result = service.update(id, ingredient);
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+          }
+        }
---
+++ src/main/java/com/foodie/api/model/entities/Ingredient.java
@@ -0,0 +1,42 @@
+package com.foodie.api.model.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "ingredients"
+        )
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ingredient implements Serializable {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "ingredientName", nullable = false, unique = true)
+  private String ingredientName;
+  @Column(name = "caloricValue", nullable = false)
+  private Double caloricValue;
+  @Column(name = "nutritionalValue", nullable = false)
+  private Double nutritionalValue;
+  @Column(name = "allNatural", nullable = false)
+  private Boolean allNatural;
+  @Column(name = "additives")
+  private String additives;
+
+}
