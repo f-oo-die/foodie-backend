4470da77d32ec765978c2cb11e6790c183da2c84
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-05-09 01:17:44 +0200

    added daily meal plan table

---
+++ src/main/java/com/foodie/api/model/entities/DailyMealPlan.java
@@ -0,0 +1,54 @@
+package com.foodie.api.model.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "daily_meal_plans"
+        )
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DailyMealPlan implements Serializable {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "date_id", nullable = false)
+  private String dateId;
+
+  @Column(name = "rating", nullable = true)
+  private Integer rating;
+
+  @ManyToOne
+  @JoinColumn(name = "breakfast_id", referencedColumnName = "id")
+  private Recipe breakfast;
+
+  @ManyToOne
+  @JoinColumn(name = "lunch_id", referencedColumnName = "id")
+  private Recipe lunch;
+
+  @ManyToOne
+  @JoinColumn(name = "dinner_id", referencedColumnName = "id")
+  private Recipe dinner;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private User user;
+}
