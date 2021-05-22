cfcf9741792b91a457326e62d0fe74219c8595b3
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-05-22 17:17:38 +0200

    fixed daily meal plan service and added create method

--- src/main/java/com/foodie/api/service/DailyMealPlanService.java
+++ src/main/java/com/foodie/api/service/DailyMealPlanService.java
@@ -15,16 +15,13 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class DailyMealPlanService {
-    private final DailyMealPlanRepository dailyMealPlanRepo;
 
-    //Get all daily meal plans
-    //get single daily meal plan
+    private final DailyMealPlanRepository dailyMealPlanRepo;
+    private final CreateDailyMealPlanService createDailyMealPlanService;
 
     public List<DailyMealPlanDto> getDailyMealPlanList(Long userId){
         List<DailyMealPlan> dailyMealPlan= dailyMealPlanRepo.findDailyMealPlanofUser(userId);
-        return dailyMealPlan.stream()
-                .map(t -> toPayload(t))
-                .collect(Collectors.toList());
+        return dailyMealPlan.stream().map(t -> toPayload(t)).collect(Collectors.toList());
     }
 
     public DailyMealPlanDto getDailyMealPlan(Long id){
@@ -35,27 +32,30 @@ public class DailyMealPlanService {
         throw new RuntimeException("DailyMealPlan with id" + id + "is not present!");
     }
 
-    /* Create daily meal plan, but requires other service
-    private DailyMealPlan fromPayload(DailyMealPlanDto payload){
-        DailyMealPlan dailyMealPlan = new DailyMealPlan();
-        dailyMealPlan.setId(dailyMealPlan.getId());
-        dailyMealPlan.setDateId(dailyMealPlan.getDateId());
-        dailyMealPlan.setRating(dailyMealPlan.getRating());
+    public DailyMealPlanDto create(Long userId){
+        DailyMealPlanDto dailyMealPlan = new DailyMealPlanDto();
+        dailyMealPlan = createDailyMealPlanService.createDailyMealPlan(userId);
         return dailyMealPlan;
     }
-    */
 
-    //Needs bugfixing, but general logic is here
+    // private DailyMealPlan fromPayload(DailyMealPlanDto payload){
+    //     DailyMealPlan dailyMealPlan = new DailyMealPlan();
+    //     dailyMealPlan.setDateId(dailyMealPlan.getDateId());
+    //     dailyMealPlan.setRating(dailyMealPlan.getRating());
+    //     dailyMealPlan.setBreakfast(RecipeService.fromPayload(payload.getBreakfast()));
+    //     dailyMealPlan.setLunch(RecipeService.fromPayload(payload.getLunch()));
+    //     dailyMealPlan.setDinner(RecipeService.fromPayload(payload.getDinner()));
+    //     return dailyMealPlan;
+    // }
+
     public static DailyMealPlanDto toPayload(DailyMealPlan dailyMealPlan){
         DailyMealPlanDto payload = new DailyMealPlanDto();
         payload.setId(dailyMealPlan.getId());
         payload.setDateId(dailyMealPlan.getDateId());
         payload.setRating(dailyMealPlan.getRating());
-        payload.setBreakfast(dailyMealPlan.toPayload(dailyMealPlan.getBreakfast()));
-        payload.setDinner(dailyMealPlan.toPayload(dailyMealPlan.getDinner()));
-        payload.setLunch(dailyMealPlan.toPayload(dailyMealPlan.getLunch()));
+        payload.setBreakfast(RecipeService.toPayload(dailyMealPlan.getBreakfast()));
+        payload.setLunch(RecipeService.toPayload(dailyMealPlan.getLunch()));
+        payload.setDinner(RecipeService.toPayload(dailyMealPlan.getDinner()));
         return payload;
     }
-
-
 }
