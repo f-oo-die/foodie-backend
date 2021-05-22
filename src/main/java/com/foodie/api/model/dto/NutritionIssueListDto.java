bfd6e296163cdf21faf83662f5a47694d30f0fad
Armin Salihovic <armin.salihovic@live.com>
2021-05-22 11:52:39 +0200

    add Nutrition Issue List Dto

---
+++ src/main/java/com/foodie/api/model/dto/NutritionIssueListDto.java
@@ -0,0 +1,18 @@
+package com.foodie.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NutritionIssueListDto {
+    private Long id;
+    private NutritionIssueDto nutritionIssue;
+
+    // todo: uncomment the line below when we create User crud . . .
+    // private UserDto user;
+}
