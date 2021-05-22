d74692104a346bec8a1bb63fe69c8b0d6d428cfa
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-05-22 17:15:29 +0200

    modified mapping path and added create endpoint in daily meal plan controller

--- src/main/java/com/foodie/api/controller/DailyMealPlanController.java
+++ src/main/java/com/foodie/api/controller/DailyMealPlanController.java
@@ -1,27 +1,31 @@
 package com.foodie.api.controller;
 
 
+import java.util.Collection;
+
 import com.foodie.api.model.dto.DailyMealPlanDto;
 import com.foodie.api.service.DailyMealPlanService;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/ingredients")
+@RequestMapping("/meal-planning")
 @RequiredArgsConstructor
 public class DailyMealPlanController {
     private final DailyMealPlanService service;
 
     @GetMapping
-    public ResponseEntity<Collection<DailyMealPlanDto>>getDailyMealPlanList(@PathVariable Long userId){
-
+    public ResponseEntity<Collection<DailyMealPlanDto>> getDailyMealPlanList(
+        @PathVariable Long userId
+    ){
         Collection<DailyMealPlanDto> allPlans = service.getDailyMealPlanList(userId);
 
         return ResponseEntity.status(HttpStatus.OK).body(allPlans);
@@ -29,10 +33,19 @@ public class DailyMealPlanController {
 
     @GetMapping("{id}")
     public ResponseEntity<DailyMealPlanDto> get(
-            @PathVariable Long id
+        @PathVariable Long id
     ){
         DailyMealPlanDto result = service.getDailyMealPlan(id);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
+
+    @PostMapping
+    public ResponseEntity<DailyMealPlanDto> save(
+        @PathVariable Long userId
+    ){
+        DailyMealPlanDto result = service.create(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
 }
 
