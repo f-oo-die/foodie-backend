13ca3c2f9ba5b42f07187bf4cb5cbeefb6a520fa
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-06-05 13:07:12 +0200

    removed shopping ingredient join table

--- src/main/java/com/foodie/api/repository/ShoppingIngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodie.api.repository;
-
-import com.foodie.api.model.entities.ShoppingIngredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ShoppingIngredientRepository extends JpaRepository<ShoppingIngredient, Long> {
-
-}
