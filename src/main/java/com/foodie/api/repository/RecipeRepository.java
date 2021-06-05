4bfb5504903df11e3a8e9988e5e7989ad7f96cc5
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-06-05 11:32:42 +0200

    added count to recipe query

--- src/main/java/com/foodie/api/repository/RecipeRepository.java
+++ src/main/java/com/foodie/api/repository/RecipeRepository.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
   
-  @Query(value = "SELECT r.id, r.title, r.preparation, r.num_of_calories, r.type_of_meal FROM recipes r WHERE r.type_of_meal = ?1 AND r.calorie_status = ?2", nativeQuery = true)
+  @Query(value = "SELECT r.id, r.title, r.preparation, r.num_of_calories, r.type_of_meal, r.calorie_status, r.recipe_count FROM recipes r WHERE r.type_of_meal = ?1 AND r.calorie_status = ?2", nativeQuery = true)
   List<Recipe> findAllRecipesByTypeOfMealAndStatus(Integer typeOfMeal, Integer status);
 }
