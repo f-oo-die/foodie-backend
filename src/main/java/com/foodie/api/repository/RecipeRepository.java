93fcfe2ad76f2c5a44901cc5ea7002a3e1ade302
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-06-05 19:36:50 +0200

    fixed recipe query

--- src/main/java/com/foodie/api/repository/RecipeRepository.java
+++ src/main/java/com/foodie/api/repository/RecipeRepository.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
   
-  @Query(value = "SELECT r.id, r.title, r.preparation, r.num_of_calories, r.type_of_meal, r.calorie_status, r.recipe_count FROM recipes r WHERE r.type_of_meal = ?1 AND r.calorie_status = ?2", nativeQuery = true)
-  List<Recipe> findAllRecipesByTypeOfMealAndStatus(Integer typeOfMeal, Integer status);
+  @Query(value = "SELECT distinct r.id, r.title, r.preparation, r.num_of_calories, r.type_of_meal, r.calorie_status, r.recipe_count FROM recipes r, recipes_nutrition_issues n WHERE r.type_of_meal = ?1 AND r.calorie_status = ?2 AND n.nutrition_issues_id in (?3)", nativeQuery = true)
+  List<Recipe> findAllRecipesByTypeOfMealAndStatus(Integer typeOfMeal, Integer status, List<Long> nutritionIssueIds);
 }
