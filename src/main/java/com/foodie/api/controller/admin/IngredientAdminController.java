dbbda6eab29f57da3641985da99e2354f159ef70
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-06-05 16:50:19 +0200

    added delete mapping

--- src/main/java/com/foodie/api/controller/admin/IngredientAdminController.java
+++ src/main/java/com/foodie/api/controller/admin/IngredientAdminController.java
@@ -8,6 +8,7 @@ import com.foodie.api.service.IngredientService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,13 @@ public class IngredientAdminController {
   ){
       IngredientDto result = service.update(id, ingredient);
       return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> delete(
+      @PathVariable Long id
+   ) {
+      service.delete(id);
+      return ResponseEntity.noContent().build();
     }
 }
