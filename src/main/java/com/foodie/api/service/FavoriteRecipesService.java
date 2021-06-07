d278af53ec1d94b42b35eca3df3743634cdfe7ae
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-06-07 17:51:47 +0200

    implemented add and remove favorite recipes

--- src/main/java/com/foodie/api/service/FavoriteRecipesService.java
+++ src/main/java/com/foodie/api/service/FavoriteRecipesService.java
@@ -2,11 +2,13 @@ package com.foodie.api.service;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.foodie.api.model.dto.RecipeDto;
 import com.foodie.api.model.entities.Recipe;
 import com.foodie.api.repository.RecipeRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.stereotype.Service;
 
@@ -23,15 +25,26 @@ public class FavoriteRecipesService {
     return favoriteRecipes.stream().map((t) -> RecipeService.toPayload(t)).collect(Collectors.toList());
   }
 
-  public Object save(Long userId, Long recipeId) {
-    return null;
+  @Transactional
+  public RecipeDto save(Long userId, Long recipeId) {
+    Optional<Recipe> favoriteRecipe = recipeRepository.findById(recipeId);
+        if (favoriteRecipe.isPresent()){
+            recipeRepository.saveRelationship(userId, recipeId);
+            return RecipeService.toPayload(favoriteRecipe.get());
+        }
+        throw new RuntimeException("Recipe with id " + recipeId + " failed to add to favorite!");
   }
 
   public RecipeDto getFavoriteRecipe(Long userId, Long recipeId) {
-    return null;
+    Optional<Recipe> favoriteRecipe = recipeRepository.findById(recipeId);
+        if (favoriteRecipe.isPresent()){
+            return RecipeService.toPayload(favoriteRecipe.get());
+        }
+        throw new RuntimeException("Recipe with id " + recipeId + " is not added to favorite!");
   }
 
+  @Transactional
   public void delete(Long userId, Long recipeId) {
+    recipeRepository.deleteRelationship(userId, recipeId);
   }
-
 }
