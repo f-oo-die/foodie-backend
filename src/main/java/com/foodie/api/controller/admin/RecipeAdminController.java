57f6cdfc90c971254c5a93741212b8ac930ed9e2
Armin Salihovic <armin.salihovic@live.com>
2021-06-09 15:18:33 +0200

    update image on profile to nullable, add delete mappings for admin controllers

--- src/main/java/com/foodie/api/controller/admin/RecipeAdminController.java
+++ src/main/java/com/foodie/api/controller/admin/RecipeAdminController.java
@@ -7,13 +7,7 @@ import com.foodie.api.service.RecipeService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -57,5 +51,13 @@ public class RecipeAdminController {
     RecipeDto result = service.update(id, recipe);
 
     return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> delete(
+          @PathVariable Long id
+  ) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
   }
 }
